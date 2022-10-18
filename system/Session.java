83979ec08ee92c48d0754a6a9293bdecc7f611db
g-nitin <niting1209@gmail.com>
2022-10-18 08:52:00 -0400

    adding structure and Session.java

---
+++ system/Session.java
@@ -0,0 +1,61 @@
+package system;
+import java.util.UUID;
+import java.util.Date;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Cyber Council
+ */
+public class Session {
+
+    private UUID id;
+    private Date startDate;
+    private Date endDate;
+    private int durationPerWeek;
+    private int ageGroup;
+    private int availableSpots;
+    private ArrayList<Cabin> cabins;
+
+    /**
+     * 
+     * @param startDate
+     * @param endDate
+     * @param ageGroup
+     */
+    public Session(String startDate, String endDate, String ageGroup) {
+
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public boolean isAvailable() {
+        return true;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public int viewAgeGroup() {
+        return 0;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public void addCabin(Cabin cabin) {
+        ;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public String toString() {
+        return "";
+    }
+}
