0b09cff82086a543201d44f7334a99fcc060e871
ossendorfluca <luca-rita.ossendorf@stud.uni-bamberg.de>
2022-10-18 15:04:00 -0400

    added UI class stub

---
+++ system/Schedule.java
@@ -0,0 +1,15 @@
+package system;
+
+import java.util.ArrayList;
+
+public class Schedule {
+    private ArrayList<Activity> activities;
+
+    public Schedule(){}
+
+    public void addActivity(){}
+    public void removeActivity(){}
+    public String toString(){
+        return "this is a schedule";
+    }
+}
---
+++ system/UI.java
@@ -0,0 +1,27 @@
+package system;
+
+import java.util.Scanner;
+
+public class UI {
+    private Scanner scanner;
+    private CampSystemFACADE campSystem;
+
+    public void run(){}
+    private void displayMainMenu(){}
+    private int getUserCommand(){
+        return 1;
+    };
+    private void createAccount(){}
+    private void login(){}
+    private void addCamper(){}
+    private void finalizePayment(){}
+    private void printGeneralInformation(){}
+    private void printFAQ(){}
+    private void logout(){}
+    private void sessionSignup(){}
+    private void askToAcceptWaiver(){}
+    public static void main(String[] args){
+        
+    }
+    
+}
