85841b98423054bb4aef7c3c5ca4d8a732b398cd
g-nitin <niting1209@gmail.com>
2022-10-18 09:35:43 -0400

    adding Medical.java

---
+++ system/Medical.java
@@ -0,0 +1,43 @@
+package system;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Cyber Council
+ */
+public class Medical {
+    
+    private Contact doctor;
+    private ArrayList <String> allergies;
+    private ArrayList <Medication> medications;
+
+    /**
+     * @param
+     */
+    public Medical(Contact doctor) {
+
+    }
+
+    /**
+     * @param
+     */
+    public void addAllergy(String allergy) {
+
+    }
+
+    /**
+     * @param 
+     */
+    public void addMedication(Medication medication) {
+
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        return "";
+    }
+
+
+}
