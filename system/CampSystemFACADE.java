d8ac7af83824b4bb330fb8bfcfdb1bd0e34d113e
ossendorfluca <luca-rita.ossendorf@stud.uni-bamberg.de>
2022-10-20 10:45:13 -0400

    stuff, mostly in the UI

--- system/CampSystemFACADE.java
+++ system/CampSystemFACADE.java
@@ -1,7 +1,7 @@
 package system;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class CampSystemFACADE {
     private User currentUser;
@@ -10,8 +10,8 @@ public class CampSystemFACADE {
 
     public CampSystemFACADE(){}
     public boolean login(String userName, String password){return true;}
-    public boolean createAccount(String userName, String password, String email, String lastName, String firstName, String phoneNumber, String preferredContact, Date birthday, String address){return true;}
-    public boolean addCamper(String firstName, String lastName, String relatioship, Date birthday, String firstNameGuardian, String lastNameGuardian,  String phoneNumberGuardian, String addressGuardian, String firstNameEmergency, String lastNameEmergency, String phoneEmergency, String addressEmergency, String firstNameDoctor, String lastNameDoctor, String phoneDoctor, String addressDoctor, ArrayList<String> allergies, ArrayList<Medication> medication, ArrayList<String> notes){return true;}
+    public boolean createAccount(String userName, String password, String email, String lastName, String firstName, String phoneNumber, String preferredContact, LocalDate birthday, String address){return true;}
+    public boolean addCamper(String firstName, String lastName, String relatioship, LocalDate birthday, String firstNameGuardian, String lastNameGuardian,  String phoneNumberGuardian, String addressGuardian, String firstNameEmergency, String lastNameEmergency, String phoneEmergency, String addressEmergency, String firstNameDoctor, String lastNameDoctor, String phoneDoctor, String addressDoctor, ArrayList<String> allergies, ArrayList<Medication> medication, ArrayList<String> notes){return true;}
     public boolean sessionSignup(Camper camper, Session session){return true;}
     public void logout(){}
 }
--- system/UI.java
+++ system/UI.java
@@ -1,5 +1,6 @@
 package system;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class UI {
@@ -8,6 +9,7 @@ public class UI {
 
     public UI(){
         scanner = new Scanner(System.in);
+        campSystem = new CampSystemFACADE();
     }
 
     public void run(){}
@@ -15,9 +17,47 @@ public class UI {
     private int getUserCommand(){
         return 1;
     };
-    private void createAccount(){}
+    private void createAccount(){
+        while(true){
+            System.out.print("\nEnter your first name: ");
+            String firstName = scanner.nextLine();
+            System.out.print("\nEnter your last name: ");
+            String lastName = scanner.nextLine();
+            System.out.print("\nEnter a username: ");
+            String username = scanner.nextLine();
+            System.out.print("\nEnter a password: ");
+            String password = scanner.nextLine();
+            System.out.print("\nEnter your email address: ");
+            String email = scanner.nextLine();
+            System.out.print("\nEnter your phone number: ");
+            String phoneNumber = scanner.nextLine();
+            System.out.print("\nEnter your preferred method of contact (phone or email): ");
+            String preferredContact = scanner.nextLine();
+            System.out.print("\nEnter your birthday (format: yyyy-mm-dd): ");
+            String birthdayString = scanner.nextLine();
+            LocalDate birthday = LocalDate.parse(birthdayString);
+            System.out.print("\nEnter your address: ");
+            String address = scanner.nextLine();
+            if(campSystem.createAccount(username, password, email, lastName, firstName, phoneNumber, preferredContact, birthday, address) == true){
+                System.out.println("\nAccount created successfully.");
+                break;
+            }
+            System.out.println("\nInvalid input. Start over.");
+        }
+        
+    }
     private void login(){
-
+        while(true){
+            System.out.print("Username: ");
+            String username = scanner.nextLine();
+            System.out.print("Password: ");
+            String password = scanner.nextLine();
+            if(campSystem.login(username, password) == true){
+                System.out.println("Login successful.");
+                break;
+            }
+            System.out.println("Username or password invalid. Try again.");
+        }
     }
     private void addCamper(){}
     private void finalizePayment(){}
