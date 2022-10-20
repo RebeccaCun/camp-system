e129d3815580c1a13c0145355b8348ffe75cae5b
ossendorfluca <luca-rita.ossendorf@stud.uni-bamberg.de>
2022-10-20 09:02:56 -0400

    added stuff to UI

--- system/UI.java
+++ system/UI.java
@@ -6,13 +6,19 @@ public class UI {
     private Scanner scanner;
     private CampSystemFACADE campSystem;
 
+    public UI(){
+        scanner = new Scanner(System.in);
+    }
+
     public void run(){}
     private void displayMainMenu(){}
     private int getUserCommand(){
         return 1;
     };
     private void createAccount(){}
-    private void login(){}
+    private void login(){
+
+    }
     private void addCamper(){}
     private void finalizePayment(){}
     private void printGeneralInformation(){}
