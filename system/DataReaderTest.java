484b75f8c9fd840dda1067415429b156ba92fe11
g-nitin <niting1209@gmail.com>
2022-11-04 16:53:34 -0400

    starting to test DataReader

--- system/DataReaderTest.java
+++ system/DataReaderTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.rmi.server.UID;
 import java.time.LocalDate;
 import java.util.ArrayList;
-
+import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.Test;
 class DataReaderTest {
 
     private UserList ul = UserList.getInstance();
+    private CamperList cl = CamperList.getInstance();
     private ArrayList<User> users = ul.getUsers();
+    private ArrayList<Camper> campers = CamperList.getInstance().getCampers();
 
     @BeforeEach
     public void setup() {
@@ -39,6 +41,8 @@ class DataReaderTest {
 	public void tearDown() {
         UserList.getInstance().getUsers().clear();
 		DataWriter.saveUsers();
+        CamperList.getInstance().getCampers().clear();
+		DataWriter.saveCampers();
 	}
 
     @Test
@@ -67,5 +71,37 @@ class DataReaderTest {
     void testEmptyCampers() {
         users = DataReader.getAllUsers();
         assertEquals(0, users.get(0).getCampers().size());
+    }
+
+    @Test
+    void testCamperAndUserID() {
+        
+        // Create User
+        User u = new User("Mikey", "White",
+                "mWhite");
+        u.addBirthday(LocalDate.parse("2022-10-20"));
+        u.setType(Type.PARENT);
+   
+        // Create new Camper for the User
+        Camper c = new Camper("Long", "Kam", LocalDate.parse("2022-10-21"));
+        // Add Medical Info to the Camper
+        Medical med = new Medical(new Contact("doc", "tor", "80-479-3939", "everywhere"));
+        c.addMedical(med);
+        u.addCamper(c);
+
+        // Save to Lists
+        ul.addUser(u);
+        cl.addCamper(c);
+        DataWriter.saveUsers();
+        DataWriter.saveCampers();
+
+        // Get the Users and Campers
+        campers = DataReader.getAllCampers();
+        users = DataReader.getAllUsers();
+    
+        // Check if the UUIDs are same
+        assertEquals(0,
+            campers.get(0).getUUID().compareTo( 
+            users.get(1).getCampers().get(0).getUUID() ));
     }
 }
