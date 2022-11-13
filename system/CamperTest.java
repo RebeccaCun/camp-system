1472b229dcadfbf078646ca619937d8c3bd45d07
CharSurry <csurry@email.sc.edu>
2022-11-13 17:02:05 -0500

    more

--- system/CamperTest.java
+++ system/CamperTest.java
@@ -10,10 +10,12 @@ import org.junit.jupiter.api.Test;
 
 public class CamperTest {
     public Camper camper;
+    public Camper another;
 
     @BeforeEach
     public void setup(){
         camper = new Camper("Jannie", "Smith", LocalDate.parse("2012-03-08"));
+        another = new Camper("George","Beard", LocalDate.parse("12"));
         //DataWriter.saveUsers();
     }
     @AfterEach
@@ -33,7 +35,16 @@ public class CamperTest {
         }
         assertTrue(valid);
     }
-
+    @Test
+    void testFirstNameValid2() {
+        String hasGeorge = another.getFirstName();
+        boolean valid = false;
+        if(hasGeorge == "George")
+        {
+            valid = true;
+        }
+        assertTrue(valid);
+    }
     @Test
     void testLastNameValid() {
         String hasSmith = camper.getLastName();
@@ -42,13 +53,31 @@ public class CamperTest {
             valid = true;
         assertTrue(valid);
     }
+    @Test
+    void testLastNameValid2() {
+        String hasBeard = another.getLastName();
+        boolean valid = false;
+        if(hasBeard == "Beard")
+        {
+            valid = true;
+        }
+        assertTrue(valid);
+    }
 
     @Test
-    void testValidBirthday(){
+    void testBirthday(){
         LocalDate bday = camper.getBirthday();
         boolean valid = false;
         if(bday.equals(LocalDate.parse("2012-03-08")))
             valid = true;
         assertTrue(valid);
     }
+    /*@Test     ?????
+    void testBirthday2(){
+        LocalDate bday = camper.getBirthday();
+        boolean valid = false;
+        if(bday.equals(LocalDate.parse("12")))
+            valid = true;
+        assertTrue(valid);
+    }*/
 }
